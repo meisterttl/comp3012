3ec066165d3cae3a8bd258a7f7d387e90e4d740f
Taylor Lee <meisterttl@gmail.com>
2025-12-10 02:08:39 -0800

    Clean up math helper file, adding lab objective and desired output

--- lab2/main.js
+++ lab2/main.js
@@ -1,12 +1,29 @@
+/* 
+Do the following: node main.js 10 5 2 3
+1. Your program will be run like you see in the box above. The format is: x1 y1 x2 y2
+2. Have a file called **mathHelpers.js** which contains a squareRoot function, a square function, and a distance function which uses internally your squareRoot and square function to calculate distance. 
+Export your distance function from this file. 
+3. Have a file called main.js which uses process.argv to get the arguments passed in from the command prompt (terminal). 
+Import your mathHelpers.js distance function into this file.
+4. Inside your main.js, you should have a function called **processInput**, passing to it what the user typed in (5 4 3 2). 
+**processInput** should take the userInput and *write it to a file* called points.txt. 
+points.txt should be saved in a folder named dataPoints. 
+The dataPoints folder does not exist so you will need to create it using the fs.mkdir function in Node. 
+After having written to the file, show a message in the console stating: "Content saved" through console. 
+5. After showing the "Content saved" message, you should use fs.appendFile to add the distance calculation to the end of your file. 
+You must add the following message to the end of the file:
+The distance between your two points: (10,5), (2,3) is <distance here>
+*/
+
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { EOL } from "os";
+import process from "process";
 import { calculateDistance } from "./mathHelpers.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const dirPath = path.join(__dirname, "dataPoints");
 const filePath = path.join(dirPath, "points.txt");
 
@@ -38,7 +55,10 @@ const processInput = (inputs) => {
       if (err && "ENOENT" === err.code) {
         // if the directory does not exist, create a folder using mkdir()
         fs.mkdir(dirPath, (err) => {
-          if (err) return console.log(err);
+          if (err) {
+            return console.log(err);
+          }
+
           console.log(`New directory created`);
         });
       }
@@ -50,7 +70,10 @@ const processInput = (inputs) => {
             // file already exists
             try {
               fs.appendFile(filePath, `${endMessage}${EOL}`, (err) => {
-                if (err) return console.log(err);
+                if (err) {
+                  return console.log(err);
+                }
+
                 console.log("Content saved");
               });
             } catch (err) {
@@ -58,6 +81,7 @@ const processInput = (inputs) => {
             }
             return;
           }
+
           // output any other errors that may exist
           return console.log(err);
         } else {
@@ -65,6 +89,7 @@ const processInput = (inputs) => {
           try {
             fs.writeFile(filePath, `${endMessage}${EOL}`, (err) => {
               if (err) return console.log(err);
+
               console.log("Content saved");
             });
           } finally {
@@ -78,8 +103,20 @@ const processInput = (inputs) => {
       1 === invalidCoordinates.length
         ? "The following input is invalid:"
         : "The following inputs are invalid:";
+
     console.log(`${invalidMessage} ${invalidCoordinates.join(", ")}`);
   }
 };
 
 processInput(process.argv);
+
+/*
+Output: 
+
+The distance between your two points: (10,10), (2,3) is 1
+New directory created
+Content saved
+
+The distance between your two points: (10,5), (2,3) is 5.0990195135927845
+Content saved
+*/
--- lab2/mathHelpers.js
+++ lab2/mathHelpers.js
@@ -1,12 +1,9 @@
-const squareRoot = (value) => {
-  return Math.sqrt(value);
-};
+const squareRoot = (value) => Math.sqrt(value);
 
-const square = (value) => {
-  return value * value;
-};
+const square = (value) => value * value;
 
 export const calculateDistance = (x1, x2, y1, y2) => {
   const distance = squareRoot(square(x2 - x1) + square(y2 - y1));
+
   return distance;
 };
